37bf18c7ba6ed88e46d9908048372f3516ec73a4
hwclass <harderworking.class@gmail.com>
2015-08-23 00:04:33 +0300

    Added the configuration path for the library

--- amd/example/boot.js
+++ amd/example/boot.js
@@ -6,9 +6,6 @@
 // Get all needed libraries into context
 require.config({
   paths: {
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-min',
-    templates: '../templates'
+    Booklet: '../booklet-1.1.0.js'
   }
 });
